9da7239b3c8c964437e492912782360dbe9b085b
Alex Curran <alexcurran.va@gmail.com>
2021-05-21 14:35:51 -0500

    Finish varied highlighters example using inputProperties in worklet that reference CSS custom properties

--- worklet.js
+++ worklet.js
@@ -1,16 +1,31 @@
 "use strict";
 
-registerPaint("headerHighlight", class {
+registerPaint("boxbg", class {
+    /**
+     * @returns {object} - Returns an object with context options set as key/value pairs.
+     */
     static get contextOptions() {
         return { alpha: true };
+    }
+    /**
+     * @returns {string[]} - Returns an array of strings representing CSS custom properties.
+     */
+    static get inputProperties() {
+        return ["--boxColor", "--widthSubtractor"];
     }
 
     /**
      * @param {CanvasRenderingContext2D} ctx - Canvas drawing context (actually supposed to be PaintRenderingContext2D type, but intellisense does not identify this yet.)
      * @param {number} size - Paint size property for drawn element.
+     * @param {object} props - Properties with get() method for drawn element.
      */
-    paint(ctx, size) {
-        ctx.fillStyle = "hsla(55 90% 60% / 1)";
-        ctx.fillRect(0, size.height / 3, size.width * 0.4, size.height * 0.6);
+    paint(ctx, size, props) {
+        ctx.fillStyle = props.get("--boxColor");
+        ctx.fillRect(
+            0,
+            size.height / 3,
+            size.width * 0.4 - props.get("--widthSubtractor"),
+            size.height * 0.6
+        );
     }
 });

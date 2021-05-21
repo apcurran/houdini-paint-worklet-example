340f17a9f2a2df5de1dbf6ba254c145b6b79d979
Alex Curran <alexcurran.va@gmail.com>
2021-05-21 15:24:56 -0500

    Finish complex highlights example

--- worklet.js
+++ worklet.js
@@ -13,7 +13,7 @@ registerPaint("headerHighlight", class {
      * @returns {string[]} - Returns an array of strings representing CSS custom properties.
      */
     static get inputProperties() {
-        return ["--boxColor", "--widthSubtractor"];
+        return ["--highColor"];
     }
 
     /**
@@ -23,12 +23,34 @@ registerPaint("headerHighlight", class {
      * @param {object} props - Properties with get() method for drawn element.
      */
     paint(ctx, size, props) {
-        ctx.fillStyle = props.get("--boxColor");
-        ctx.fillRect(
-            0,
-            size.height / 3,
-            size.width * 0.4 - props.get("--widthSubtractor"),
-            size.height * 0.6
-        );
+        // Set where to start the highlight and dimensions.
+        const x = 0;
+        const y = size.height * 0.3;
+        const blockWidth = size.width * 0.33;
+        const highlightHeight = size.height * 0.85;
+        const color = props.get("--highColor");
+
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(blockWidth, y);
+        ctx.lineTo(blockWidth + highlightHeight, highlightHeight);
+        ctx.lineTo(x, highlightHeight);
+        ctx.lineTo(x, y);
+        ctx.fill();
+
+        // Create the dash effect.
+        for (let i = 0; i < 4; i++) {
+            const start = i * 2;
+            const adjustedStart = start * 10;
+
+            ctx.beginPath();
+            ctx.moveTo(blockWidth + adjustedStart + 10, y);
+            ctx.lineTo(blockWidth + adjustedStart + 20, y);
+            ctx.lineTo(blockWidth + adjustedStart + 20 + highlightHeight, highlightHeight);
+            ctx.lineTo(blockWidth + adjustedStart + 10 + highlightHeight, highlightHeight);
+            ctx.lineTo(blockWidth + adjustedStart + 10, y);
+            ctx.fill();
+        }
     }
 });

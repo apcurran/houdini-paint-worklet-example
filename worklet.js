5ec88aef4e3af9c63156fcfcfde6cbea2f29912c
Alex Curran <alexcurran.va@gmail.com>
2021-05-21 15:26:24 -0500

    Fix type

--- worklet.js
+++ worklet.js
@@ -22,7 +22,7 @@ registerPaint("headerHighlight", class {
 
     /**
      * @param {CanvasRenderingContext2D} ctx - Canvas drawing context (actually supposed to be PaintRenderingContext2D type, but intellisense does not identify this yet.)
-     * @param {number} size - Paint size property for drawn element.
+     * @param {object} size - Paint size property for drawn element.
      */
     paint(ctx, size) {
         ctx.fillStyle = "hsla(55 90% 60% / 1)";
